7ddb772764c86e72196aa1e8592870017f757f38
Mark <m.hooijberg24@gmail.com>
2025-12-03 14:12:44 +0100

    Fixed some errors.

--- src/modules/static_website/route53_zones.tf
+++ src/modules/static_website/route53_zones.tf
@@ -1,6 +1,6 @@
 # Find the hosted zone in Route53 (assumes the zone exists in your account)
 data "aws_route53_zone" "primary" {
   provider     = aws.us_east_1
-  name         = local.domain_name
+  name         = var.domain_name
   private_zone = false
 }
