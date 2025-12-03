7cbba4c25ab2ec851051f8885fc3f3246a25d7e3
Mark <m.hooijberg24@gmail.com>
2025-12-03 13:57:22 +0100

    Merge ACM due to size.

--- src/modules/static_website/acm.tf
+++ src/modules/static_website/acm.tf
@@ -1,3 +1,13 @@
+# Request ACM certificate in us-east-1 (required for CloudFront)
+resource "aws_acm_certificate" "cert" {
+  provider          = aws.us_east_1
+  domain_name       = "${local.project_name}.${local.project_domain}"
+  validation_method = "DNS"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 # Complete certificate validation
 resource "aws_acm_certificate_validation" "cert_validation" {
   provider                = aws.us_east_1
--- src/modules/static_website/acm_certificates.tf
+++
@@ -1,9 +0,0 @@
-# Request ACM certificate in us-east-1 (required for CloudFront)
-resource "aws_acm_certificate" "cert" {
-  provider          = aws.us_east_1
-  domain_name       = "${local.project_name}.${local.project_domain}"
-  validation_method = "DNS"
-  lifecycle {
-    create_before_destroy = true
-  }
-}
