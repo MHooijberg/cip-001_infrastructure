c7796334a391923d539303994be43f9214072ef1
Mark <m.hooijberg24@gmail.com>
2025-12-03 13:00:18 +0100

    split s3 file.

---
+++ src/modules/lambda_notification/s3_buckets.tf
@@ -0,0 +1,12 @@
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
+# S3 bucket (for static website hosting)
+resource "aws_s3_bucket" "lambda" {
+  bucket = "${locals.project_domain}_lambda_${random_id.bucket_suffix.hex}"
+  # allow Terraform to destroy/recreate during testing; remove force_destroy in production if you want to protect objects
+  force_destroy = true
+
+  # IMPORTANT: do NOT enable "website" here — we use S3 as a private origin (not a website endpoint)
+}
